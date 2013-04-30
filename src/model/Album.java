aa20df0cd2a72bdaf5fcf1bfb86c2075252cef23
Julian Schobert <julian.schobert@gmail.com>
2013-04-30 20:38:46 +0100

    minor changes

--- src/model/Album.java
+++ src/model/Album.java
@@ -1,36 +1,36 @@
 package model;
 
 public class Album {
-	private String Name;
-	private String Interpreter;
-	private String CoverPicture;
+	private String mName;
+	private String mInterpreter;
+	private String mCoverPicture;
 	
 	//TODO Relationship one Album contains media (1:n)
 	public Album() {
 		super();
 	}
-	public String getName() {
-		return Name;
+	public String getmName() {
+		return mName;
 	}
 	public void setName(String name) {
-		Name = name;
+		mName = name;
 	}
 	public String getInterpreter() {
-		return Interpreter;
+		return mInterpreter;
 	}
 	public void setInterpreter(String interpreter) {
-		Interpreter = interpreter;
+		mInterpreter = interpreter;
 	}
 	public String getCoverPicture() {
-		return CoverPicture;
+		return mCoverPicture;
 	}
 	public void setCoverPicture(String coverPicture) {
-		CoverPicture = coverPicture;
+		mCoverPicture = coverPicture;
 	}
 
 	public String toString() {
-		return "Album: Name: " + Name + ", Interpreter: " + Interpreter
-				+ ", CoverPicture location: " + CoverPicture;
+		return "Album: Name: " + mName + ", Interpreter: " + mInterpreter
+				+ ", CoverPicture location: " + mCoverPicture;
 	}
 	
 }
